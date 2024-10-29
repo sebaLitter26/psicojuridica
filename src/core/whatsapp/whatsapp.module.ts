a0afd51d2ae33e9fb7cd7a4f3b35c344882d07fc
slitter <sebastian.litter@fluxit.com.ar>
2024-10-29 19:37:04 -0300

    deleted whatsapp

--- src/core/whatsapp/whatsapp.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { WhatsappService } from './whatsapp.service';
-
-@Global()
-@Module({
-  providers: [WhatsappService],
-  exports: [WhatsappService],
-})
-export class WhatsappModule {}
